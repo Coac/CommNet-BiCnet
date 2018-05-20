804526102461290364a0899a548b8e2f8ef42d44
Coac <levictor3000@hotmail.fr>
2018-05-20 15:29:00 +0200

    Added train_op for commnet

--- CommNet/CommNet.py
+++ CommNet/CommNet.py
@@ -4,19 +4,26 @@ from utils import *
 
 
 class CommNet:
-    def __init__(self, sess, NUM_AGENTS, VECTOR_OBS_LEN, OUTPUT_LEN):
+    def __init__(self, sess, NUM_AGENTS, VECTOR_OBS_LEN, OUTPUT_LEN, learning_rate=0.01):
         self.NUM_AGENTS = NUM_AGENTS
         self.VECTOR_OBS_LEN = VECTOR_OBS_LEN
         self.OUTPUT_LEN = OUTPUT_LEN
-
-        self.observation = tf.random_uniform(shape=(self.NUM_AGENTS, self.VECTOR_OBS_LEN), dtype="float32")
 
         self.observation = tf.placeholder(tf.float32, (self.NUM_AGENTS, self.VECTOR_OBS_LEN), name="observation")
         H1 = self.comm_step("comm_step1", self.observation)
         H2 = self.comm_step("comm_step2", H1)
         self.out = self.output_layer(H2)
 
-        self.sess = sess
+        self.sess = sess or tf.get_default_session()
+
+        self.ep_observations = []
+        self.ep_actions = []
+        self.ep_rewards = []
+
+        self.target = tf.placeholder(1, name="target")
+        self.loss = -self.dist.log_prob(self.actions) * self.target
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+        self.train_op = self.optimizer.minimize(self.loss)
 
     def comm_step(self, name, H):
         with tf.variable_scope(name):
@@ -40,24 +47,52 @@ class CommNet:
 
     def output_layer(self, H):
         with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
-            w = tf.get_variable(name='w', shape=(self.VECTOR_OBS_LEN, self.OUTPUT_LEN),
+            w = tf.get_variable(name='w_out', shape=(self.VECTOR_OBS_LEN, self.OUTPUT_LEN),
                                 initializer=tf.constant_initializer(10))
 
-            outputs = []
+            b = tf.get_variable(name='b_out', shape=self.OUTPUT_LEN, initializer=tf.constant_initializer(100))
+
+            actions = []
             for j in range(self.NUM_AGENTS):
                 h = tf.slice(H, [j, 0], [1, self.VECTOR_OBS_LEN])
 
-                outputs.append(tf.tanh(tf.matmul(h, w))[0])
-            return outputs
+                means = tf.matmul(h, w) + b
+                stds = [1.0 for i in range(self.OUTPUT_LEN)]
 
-    def act(self, observation):
+                self.dist = tf.distributions.Normal(means, stds)
+
+                action = tf.squeeze(self.dist.sample(1))
+                actions.append(action)
+
+            self.actions = tf.stack(actions)
+            return self.actions
+
+    def predict(self, observation):
         return np.array(self.sess.run(self.out, feed_dict={self.observation: observation}))
 
     def store_transition(self, s, a, r):
-        pass
+        """
+        s -- state
+        a -- actions
+        r -- scalar reward
+        """
+
+        self.ep_observations.append(s)
+        self.ep_actions.append(a)
+        self.ep_rewards.append(r)
 
     def train_step(self):
-        pass
+        for i in range(len(self.ep_observations)):
+            state = self.ep_observations[i]
+            actions = self.ep_actions[i]
+            reward = self.ep_rewards[i]
+
+            feed_dict = {self.observation: state, self.target: reward, self.actions: actions}
+            _, loss = sess.run([self.train_op, self.loss], feed_dict)
+
+        self.ep_observations = []
+        self.ep_actions = []
+        self.ep_rewards = []
 
 
 if __name__ == '__main__':
@@ -74,10 +109,8 @@ if __name__ == '__main__':
 
         sess.run(tf.global_variables_initializer())
 
-        outputs = commNet.act(np.random.random((NUM_AGENTS, VECTOR_OBS_LEN)))
+        outputs = commNet.predict(np.random.random((NUM_AGENTS, VECTOR_OBS_LEN)))
         print(outputs.shape)
-        for i in outputs:
-            print('-----')
-            print(i)
+        print(outputs)
 
         writer.close()
