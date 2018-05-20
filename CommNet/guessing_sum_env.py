0bb9cc1287b319b1729f9ba87686cd0d65ee21ff
Coac <levictor3000@hotmail.fr>
2018-05-20 11:00:48 +0200

    Add guessing sum env

---
+++ CommNet/guessing_sum_env.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class GuessingSumEnv:
+    def __init__(self, num_agents=5):
+        self.num_agents = num_agents
+        self.sum = 0
+
+    def step(self, actions):
+        if actions.shape != (self.num_agents, 1):
+            raise Exception('got input shape ', actions.shape, ' instead of ', (self.num_agents, 1))
+
+        observations = None
+        rewards = -np.abs(actions - self.sum)
+
+        done = True
+        info = None
+
+        return observations, rewards, done, info
+
+    def reset(self):
+        observations = np.random.normal(size=(self.num_agents, 1))
+        self.sum = np.sum(observations)
+        return observations
+
+    def render(self, mode='human'):
+        return
+
+    def close(self):
+        return
+
+    def seed(self, seed=None):
+        np.random.seed(seed)
+        return
+
+
+if __name__ == '__main__':
+    env = GuessingSumEnv()
+    env.seed(0)
+
+    print('obs:', env.reset())
+    actions = np.random.normal(size=(env.num_agents, 1))
+    print('actions:', actions)
+    print('rewards:', env.step(actions))
