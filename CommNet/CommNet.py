fdd6181acc30205664c7983fa5e08afb76f308c8
Coac <levictor3000@hotmail.fr>
2018-05-23 16:32:30 +0200

    DDPG WIP

--- CommNet/CommNet.py
+++ CommNet/CommNet.py
@@ -3,78 +3,49 @@ from datetime import datetime
 import numpy as np
 import tensorflow as tf
 from guessing_sum_env import *
-from utils import *
 
 HIDDEN_VECTOR_LEN = 1
+NUM_AGENTS = 5
+VECTOR_OBS_LEN = 1
+OUTPUT_LEN = 1
 
 
 class CommNet:
-    def __init__(self, sess, NUM_AGENTS, VECTOR_OBS_LEN, OUTPUT_LEN, learning_rate=0.0001):
-        self.sess = sess or tf.get_default_session()
-        self.NUM_AGENTS = NUM_AGENTS
-        self.VECTOR_OBS_LEN = VECTOR_OBS_LEN
-        self.OUTPUT_LEN = OUTPUT_LEN
+    @staticmethod
+    def base_build_network(observation):
+        H0 = CommNet.encoder(observation)
+        C0 = tf.zeros((NUM_AGENTS, HIDDEN_VECTOR_LEN), name="C0")
+        H1, C1 = CommNet.comm_step("comm_step1", H0, C0)
+        H2, C2 = CommNet.comm_step("comm_step2", H1, C1)
+        H3, _ = CommNet.comm_step("comm_step3", H2, C2, H0)
+        return H3
 
-        self.observation = tf.placeholder(tf.float32, (self.NUM_AGENTS, self.VECTOR_OBS_LEN), name="observation")
-        self.reward = tf.placeholder(tf.float32, shape=(), name="reward")
+    @staticmethod
+    def actor_build_network(name, observation):
+        with tf.variable_scope(name):
+            H = CommNet.base_build_network(observation)
+            return CommNet.actor_output_layer(H)
 
-        self.out = self._build_network(self.observation)
+    @staticmethod
+    def critic_build_network(name, observation, action):
+        with tf.variable_scope(name):
+            H = CommNet.base_build_network(observation)
+            return CommNet.critic_output_layer(H, action)
 
-        self.ep_observations = []
-        self.ep_actions = []
-        self.ep_rewards = []
-
-        self._loss()
-        self._train_op(learning_rate)
-
-
-    def _build_network(self, observation):
-        H0 = self.encoder(observation)
-        C0 = tf.zeros((self.NUM_AGENTS, HIDDEN_VECTOR_LEN), name="C0")
-        H1, C1 = self.comm_step("comm_step1", H0, C0)
-        H2, C2 = self.comm_step("comm_step2", H1, C1)
-        H3, _ = self.comm_step("comm_step3", H2, C2, H0)
-        return self.output_layer(H3)
-
-    def _loss(self):
-        with tf.name_scope("loss"):
-            alpha = 1
-            self.policy_loss = 0
-            actions = tf.reshape(self.actions, (self.NUM_AGENTS, self.OUTPUT_LEN))
-            for j in range(self.NUM_AGENTS):
-                normal_dist = self.normal_dists[j]
-                log_prob = normal_dist.log_prob(actions[j])
-                self.policy_loss -= tf.squeeze(tf.reduce_mean(log_prob)) * (self.reward - self.baseline)
-
-            self.baseline_loss = alpha * tf.square(self.reward - self.baseline)
-            self.total_loss = self.policy_loss + self.baseline_loss
-
-            tf.summary.scalar('baseline_loss', self.baseline_loss)
-            tf.summary.scalar('policy_loss', self.policy_loss)
-            tf.summary.scalar('total_loss', self.total_loss)
-            tf.summary.scalar('reward', self.reward)
-            tf.summary.scalar('baseline', self.baseline)
-
-    def _train_op(self, learning_rate):
-        with tf.name_scope("train"):
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-            gradients = self.optimizer.compute_gradients(self.total_loss)
-            clipped_gradients = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in gradients]
-            self.train_op = self.optimizer.apply_gradients(clipped_gradients)
-
-    def encoder(self, s):
+    @staticmethod
+    def encoder(s):
         H = []
-
         with tf.variable_scope("encoder", reuse=tf.AUTO_REUSE):
-            for j in range(self.NUM_AGENTS):
-                encoded = tf.layers.dense(tf.reshape(s[j], (1, self.VECTOR_OBS_LEN)), HIDDEN_VECTOR_LEN, name="dense")
+            for j in range(NUM_AGENTS):
+                encoded = tf.layers.dense(tf.reshape(s[j], (1, VECTOR_OBS_LEN)), HIDDEN_VECTOR_LEN, name="dense")
                 H.append(tf.squeeze(encoded))
             H = tf.stack(H)
-            H = tf.reshape(H, (self.NUM_AGENTS, HIDDEN_VECTOR_LEN))
+            H = tf.reshape(H, (NUM_AGENTS, HIDDEN_VECTOR_LEN))
 
         return H
 
-    def module(self, h, c):
+    @staticmethod
+    def module(h, c):
         with tf.variable_scope("module", reuse=tf.AUTO_REUSE):
             w_H = tf.get_variable(name='w_H', shape=HIDDEN_VECTOR_LEN,
                                   initializer=tf.contrib.layers.xavier_initializer())
@@ -86,13 +57,14 @@ class CommNet:
 
             return tf.tanh(tf.multiply(w_H, h) + tf.multiply(w_C, c))
 
-    def comm_step(self, name, H, C, H0_skip_con=None):
+    @staticmethod
+    def comm_step(name, H, C, H0_skip_con=None):
         with tf.variable_scope(name):
             next_H = []
-            for j in range(self.NUM_AGENTS):
+            for j in range(NUM_AGENTS):
                 h = H[j]
                 c = C[j]
-                next_h = self.module(h, c)
+                next_h = CommNet.module(h, c)
                 next_H.append(next_h)
 
             next_H = tf.stack(next_H)
@@ -102,9 +74,9 @@ class CommNet:
                 next_H = tf.add(next_H, H0_skip_con)
 
             next_C = []
-            for j1 in range(self.NUM_AGENTS):
+            for j1 in range(NUM_AGENTS):
                 next_c = []
-                for j2 in range(self.NUM_AGENTS):
+                for j2 in range(NUM_AGENTS):
                     if j1 != j2:
                         next_c.append(next_H[j2])
                 next_c = tf.reduce_mean(tf.stack(next_c), 0)
@@ -112,85 +84,46 @@ class CommNet:
 
             return next_H, tf.identity(next_C, "C")
 
-    def output_layer(self, H):
-        with tf.variable_scope("output", reuse=tf.AUTO_REUSE):
-            w_means = tf.get_variable(name='w_means_out', shape=(HIDDEN_VECTOR_LEN, self.OUTPUT_LEN),
-                                      initializer=tf.contrib.layers.xavier_initializer())
-            b_means = tf.get_variable(name='b_means_out', shape=self.OUTPUT_LEN, initializer=tf.zeros_initializer())
+    @staticmethod
+    def actor_output_layer(H):
+        with tf.variable_scope("actor_output"):
+            w_out = tf.get_variable(name='w_out', shape=(HIDDEN_VECTOR_LEN, OUTPUT_LEN),
+                                    initializer=tf.contrib.layers.xavier_initializer())
+            b_out = tf.get_variable(name='b_out', shape=OUTPUT_LEN, initializer=tf.zeros_initializer())
 
-            w_stds = tf.get_variable(name='w_stds_out', shape=(HIDDEN_VECTOR_LEN, self.OUTPUT_LEN),
-                                     initializer=tf.contrib.layers.xavier_initializer())
-            b_stds = tf.get_variable(name='b_stds_out', shape=self.OUTPUT_LEN)
-
-            tf.summary.histogram('w_means_out', w_means)
-            tf.summary.histogram('b_means_out', w_stds)
-            tf.summary.histogram('w_stds', w_stds)
-            tf.summary.histogram('b_stds', b_stds)
+            tf.summary.histogram('w_out', w_out)
+            tf.summary.histogram('b_out', b_out)
 
             actions = []
-            self.normal_dists = []
-            for j in range(self.NUM_AGENTS):
+            for j in range(NUM_AGENTS):
                 h = tf.slice(H, [j, 0], [1, HIDDEN_VECTOR_LEN])
-
-                means = tf.matmul(h, w_means) + b_means
-                means = tf.identity(means, name="means")
-
-                stds = tf.matmul(h, w_stds) + b_stds
-                stds = tf.nn.softplus(stds) + 1e-5
-                stds = tf.identity(stds, name="stds")
-
-                normal_dist = tf.distributions.Normal(loc=means, scale=stds)
-                self.normal_dists.append(normal_dist)
-
-                action = tf.squeeze(normal_dist.sample(1))
-
+                action = tf.squeeze(tf.matmul(h, w_out) + b_out)
                 actions.append(action)
+            actions = tf.stack(actions, name="actions")
 
-            self.actions = tf.stack(actions, name="actions")
+        return actions
 
-        with tf.variable_scope("baseline", reuse=tf.AUTO_REUSE):
-            self.baseline = tf.layers.dense(tf.reshape(H, (1, self.NUM_AGENTS * HIDDEN_VECTOR_LEN)), units=1,
-                                            kernel_initializer=tf.contrib.layers.xavier_initializer())
-            self.baseline = tf.squeeze(self.baseline)
+    @staticmethod
+    def critic_output_layer(H, action):
+        with tf.variable_scope("critic_output", reuse=tf.AUTO_REUSE):
+            # TODO merge action
+            # baseline = tf.layers.dense(tf.reshape(H, (1, NUM_AGENTS * HIDDEN_VECTOR_LEN)), units=1,
+            #                                    kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+
+            baseline = tf.layers.dense(
+                tf.reshape([H, action], (1, NUM_AGENTS * HIDDEN_VECTOR_LEN + NUM_AGENTS * OUTPUT_LEN)), units=1,
+                kernel_initializer=tf.contrib.layers.xavier_initializer())
+
+            baseline = tf.squeeze(baseline)
 
             tf.summary.histogram("w_baseline", tf.get_variable("dense/kernel"))
 
-        return self.actions
-
-    def predict(self, observation):
-        return np.array(self.sess.run(self.out, feed_dict={self.observation: observation}))
-
-    def store_transition(self, s, a, r):
-        """
-        s -- state
-        a -- actions
-        r -- scalar reward
-        """
-
-        self.ep_observations.append(s)
-        self.ep_actions.append(a)
-        self.ep_rewards.append(r)
-
-    def train_step(self):
-        for i in range(len(self.ep_observations)):
-            state = self.ep_observations[i]
-            actions = self.ep_actions[i]
-            reward = self.ep_rewards[i]
-
-            feed_dict = {self.observation: state, self.reward: reward, self.actions: actions}
-            sess.run([self.train_op], feed_dict)
-
-        self.ep_observations = []
-        self.ep_actions = []
-        self.ep_rewards = []
+            return baseline
 
 
 if __name__ == '__main__':
     tf.set_random_seed(42)
-
-    NUM_AGENTS = 5
-    VECTOR_OBS_LEN = 1
-    OUTPUT_LEN = 1
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
---
+++ CommNet/CommNet2.py
@@ -0,0 +1,345 @@
+""" 
+Implementation of DDPG - Deep Deterministic Policy Gradient https://github.com/pemami4911/deep-rl
+Modified by Coac for CommNet implementation https://github.com/Coac/CommNet-BiCnet
+"""
+import argparse
+import pprint as pp
+from datetime import datetime
+
+import numpy as np
+import tensorflow as tf
+from guessing_sum_env import *
+from replay_buffer import ReplayBuffer
+
+from CommNet import CommNet
+
+HIDDEN_VECTOR_LEN = 1
+NUM_AGENTS = 5
+VECTOR_OBS_LEN = 1
+OUTPUT_LEN = 1
+
+
+# ===========================
+#   Actor and Critic DNNs
+# ===========================
+
+class ActorNetwork(object):
+    """
+    Input to the network is the state, output is the action
+    under a deterministic policy.
+
+    The output layer activation is a tanh to keep the action
+    between -action_bound and action_bound
+    """
+
+    def __init__(self, sess, state_dim, action_dim, action_bound, learning_rate, tau, batch_size):
+        self.sess = sess
+        self.s_dim = state_dim
+        self.a_dim = action_dim
+        self.action_bound = action_bound
+        self.learning_rate = learning_rate
+        self.tau = tau
+        self.batch_size = batch_size
+
+        # Actor Network
+        self.inputs, self.out = self.create_actor_network("actor_network")
+
+        self.network_params = tf.trainable_variables()
+
+        # Target Network
+        self.target_inputs, self.target_out = self.create_actor_network("target_actor_network")
+
+        self.target_network_params = tf.trainable_variables()[
+                                     len(self.network_params):]
+
+        # Op for periodically updating target network with online network
+        # weights
+        self.update_target_network_params = \
+            [self.target_network_params[i].assign(tf.multiply(self.network_params[i], self.tau) +
+                                                  tf.multiply(self.target_network_params[i], 1. - self.tau))
+             for i in range(len(self.target_network_params))]
+
+        # This gradient will be provided by the critic network
+        self.action_gradient = tf.placeholder(tf.float32, self.a_dim)
+
+        self.out = tf.reshape(self.out, self.action_gradient.shape)
+
+        # Combine the gradients here
+        self.unnormalized_actor_gradients = tf.gradients(
+            self.out, self.network_params, -self.action_gradient)
+        self.actor_gradients = list(map(lambda x: tf.div(x, self.batch_size), self.unnormalized_actor_gradients))
+
+        # Optimization Op
+        self.optimize = tf.train.AdamOptimizer(self.learning_rate). \
+            apply_gradients(zip(self.actor_gradients, self.network_params))
+
+        self.num_trainable_vars = len(
+            self.network_params) + len(self.target_network_params)
+
+    def create_actor_network(self, name):
+        inputs = tf.placeholder(tf.float32, shape=(NUM_AGENTS, VECTOR_OBS_LEN))
+        out = CommNet.actor_build_network(name, inputs)
+        return inputs, out
+
+    def train(self, inputs, a_gradient):
+        self.sess.run(self.optimize, feed_dict={
+            self.inputs: inputs,
+            self.action_gradient: a_gradient
+        })
+
+    def predict(self, inputs):
+        return self.sess.run(self.out, feed_dict={
+            self.inputs: inputs
+        })
+
+    def predict_target(self, inputs):
+        return self.sess.run(self.target_out, feed_dict={
+            self.target_inputs: inputs
+        })
+
+    def update_target_network(self):
+        self.sess.run(self.update_target_network_params)
+
+    def get_num_trainable_vars(self):
+        return self.num_trainable_vars
+
+
+class CriticNetwork(object):
+    """
+    Input to the network is the state and action, output is Q(s,a).
+    The action must be obtained from the output of the Actor network.
+
+    """
+
+    def __init__(self, sess, state_dim, action_dim, learning_rate, tau, gamma, num_actor_vars):
+        self.sess = sess
+        self.s_dim = state_dim
+        self.a_dim = action_dim
+        self.learning_rate = learning_rate
+        self.tau = tau
+        self.gamma = gamma
+
+        # Create the critic network
+        self.inputs, self.action, self.out = self.create_critic_network("critic_network")
+
+        self.network_params = tf.trainable_variables()[num_actor_vars:]
+
+        # Target Network
+        self.target_inputs, self.target_action, self.target_out = self.create_critic_network("target_critic_network")
+
+        self.target_network_params = tf.trainable_variables()[(len(self.network_params) + num_actor_vars):]
+
+        # Op for periodically updating target network with online network
+        # weights with regularization
+        self.update_target_network_params = \
+            [self.target_network_params[i].assign(tf.multiply(self.network_params[i], self.tau) \
+                                                  + tf.multiply(self.target_network_params[i], 1. - self.tau))
+             for i in range(len(self.target_network_params))]
+
+        # Network target (y_i)
+        self.predicted_q_value = tf.placeholder(tf.float32, ())
+
+        # Define loss and optimization Op
+        self.loss = tf.losses.mean_squared_error(self.predicted_q_value, self.out)
+        self.optimize = tf.train.AdamOptimizer(
+            self.learning_rate).minimize(self.loss)
+
+        # Get the gradient of the net w.r.t. the action.
+        # For each action in the minibatch (i.e., for each x in xs),
+        # this will sum up the gradients of each critic output in the minibatch
+        # w.r.t. that action. Each output is independent of all
+        # actions except for one.
+        self.action_grads = tf.gradients(self.out, self.action)
+
+    def create_critic_network(self, name):
+        inputs = tf.placeholder(tf.float32, shape=(NUM_AGENTS, VECTOR_OBS_LEN))
+        action = tf.placeholder(tf.float32, shape=(NUM_AGENTS, OUTPUT_LEN))
+
+        out = CommNet.critic_build_network(name, inputs, action)
+        return inputs, action, out
+
+    def train(self, inputs, action, predicted_q_value):
+        return self.sess.run([self.out, self.optimize], feed_dict={
+            self.inputs: inputs,
+            self.action: action,
+            self.predicted_q_value: predicted_q_value
+        })
+
+    def predict(self, inputs, action):
+        return self.sess.run(self.out, feed_dict={
+            self.inputs: inputs,
+            self.action: action
+        })
+
+    def predict_target(self, inputs, action):
+        return self.sess.run(self.target_out, feed_dict={
+            self.target_inputs: inputs,
+            self.target_action: action
+        })
+
+    def action_gradients(self, inputs, actions):
+        return self.sess.run(self.action_grads, feed_dict={
+            self.inputs: inputs,
+            self.action: actions
+        })
+
+    def update_target_network(self):
+        self.sess.run(self.update_target_network_params)
+
+
+# ===========================
+#   Tensorflow Summary Ops
+# ===========================
+
+def build_summaries():
+    episode_reward = tf.Variable(0.)
+    tf.summary.scalar("Reward", episode_reward)
+    episode_ave_max_q = tf.Variable(0.)
+    tf.summary.scalar("Qmax Value", episode_ave_max_q)
+
+    summary_vars = [episode_reward, episode_ave_max_q]
+    summary_ops = tf.summary.merge_all()
+
+    return summary_ops, summary_vars
+
+
+# ===========================
+#   Agent Training
+# ===========================
+
+def train(sess, env, args, actor, critic):
+    # Set up summary Ops
+    summary_ops, summary_vars = build_summaries()
+
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter("summaries/" + datetime.now().strftime('%d-%m-%y %H%M'), sess.graph)
+
+    # Initialize target network weights
+    actor.update_target_network()
+    critic.update_target_network()
+
+    # Initialize replay memory
+    replay_buffer = ReplayBuffer(int(args['buffer_size']), int(args['random_seed']))
+
+    for i in range(int(args['max_episodes'])):
+        state = env.reset()
+
+        ep_reward = 0
+        ep_ave_max_q = 0
+
+        for j in range(int(args['max_episode_len'])):
+
+            if args['render_env']:
+                env.render()
+
+            action = actor.predict(state)
+
+            state2, reward, done, info = env.step(action)
+            reward = np.sum(reward)
+
+            replay_buffer.add(state, action, reward,
+                              done, state2)
+
+            # Keep adding experience to the memory until
+            # there are at least minibatch size samples
+            if True or replay_buffer.size() > int(args['minibatch_size']):
+                s_batch, a_batch, r_batch, t_batch, s2_batch = \
+                    replay_buffer.sample_batch(int(args['minibatch_size']))
+
+                # TODO
+                # Calculate targets
+                # target_q = critic.predict_target(
+                #     s2_batch, actor.predict_target(s2_batch))
+
+                target_q = tf.zeros((1))
+
+                # Update the critic given the targets
+                predicted_q_value, _ = critic.train(
+                    s_batch, a_batch, r_batch)
+
+                ep_ave_max_q += predicted_q_value
+
+                # Update the actor policy using the sampled gradient
+                a_outs = actor.predict(s_batch)
+                grads = critic.action_gradients(s_batch, a_outs)
+                actor.train(s_batch, grads[0])
+
+                # Update target networks
+                actor.update_target_network()
+                critic.update_target_network()
+
+            state = state2
+            ep_reward += reward
+
+            if done:
+                summary_str = sess.run(summary_ops, feed_dict={
+                    summary_vars[0]: ep_reward,
+                    summary_vars[1]: ep_ave_max_q / float(j + 1)
+                })
+
+                writer.add_summary(summary_str, i)
+                writer.flush()
+
+                print('| Reward: {:.4f} | Episode: {:d} | Qmax: {:.4f}'.format(ep_reward,
+                                                                               i, (ep_ave_max_q / float(j + 1))))
+                break
+
+
+def main(args):
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        env = GuessingSumEnv()
+        env.seed(0)
+
+        np.random.seed(int(args['random_seed']))
+        tf.set_random_seed(int(args['random_seed']))
+        env.seed(int(args['random_seed']))
+
+        state_dim = (NUM_AGENTS, VECTOR_OBS_LEN)
+        action_dim = (NUM_AGENTS, OUTPUT_LEN)
+        action_bound = 10
+
+        actor = ActorNetwork(sess, state_dim, action_dim, action_bound,
+                             float(args['actor_lr']), float(args['tau']),
+                             int(args['minibatch_size']))
+
+        critic = CriticNetwork(sess, state_dim, action_dim,
+                               float(args['critic_lr']), float(args['tau']),
+                               float(args['gamma']),
+                               actor.get_num_trainable_vars())
+
+        train(sess, env, args, actor, critic)
+
+        if args['use_gym_monitor']:
+            env.monitor.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='provide arguments for DDPG agent')
+
+    # agent parameters
+    parser.add_argument('--actor-lr', help='actor network learning rate', default=0.0001)
+    parser.add_argument('--critic-lr', help='critic network learning rate', default=0.001)
+    parser.add_argument('--gamma', help='discount factor for critic updates', default=0.99)
+    parser.add_argument('--tau', help='soft target update parameter', default=0.001)
+    parser.add_argument('--buffer-size', help='max size of the replay buffer', default=1000000)
+    parser.add_argument('--minibatch-size', help='size of minibatch for minibatch-SGD', default=1)
+
+    # run parameters
+    parser.add_argument('--env', help='choose the gym env- tested on {Pendulum-v0}', default='Pendulum-v0')
+    parser.add_argument('--random-seed', help='random seed for repeatability', default=1234)
+    parser.add_argument('--max-episodes', help='max num of episodes to do while training', default=50000)
+    parser.add_argument('--max-episode-len', help='max length of 1 episode', default=1000)
+    parser.add_argument('--render-env', help='render the gym env', action='store_true')
+    parser.add_argument('--use-gym-monitor', help='record gym results', action='store_true')
+    parser.add_argument('--monitor-dir', help='directory for storing gym results', default='./results/gym_ddpg')
+    parser.add_argument('--summary-dir', help='directory for storing tensorboard info', default='./results/tf_ddpg')
+
+    parser.set_defaults(render_env=False)
+    parser.set_defaults(use_gym_monitor=True)
+
+    args = vars(parser.parse_args())
+
+    pp.pprint(args)
+
+    main(args)
